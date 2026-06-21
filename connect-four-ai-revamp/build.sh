b131179ff5c2517c41af03f555074f271fb5f394
Aiyaz Mostofa <aiyaz5040@gmail.com>
2026-06-21 03:25:18 -0500

    Woops, bear there for no reason!

--- connect-four-ai-revamp/build.sh
+++ connect-four-ai-revamp/build.sh
@@ -1,2 +1,2 @@
 mkdir -p bin
-bear -- emcc -o bin/engine.mjs engine.cpp -s "NO_EXIT_RUNTIME=1" -s "EXPORTED_RUNTIME_METHODS=['ccall']"
+emcc -o bin/engine.mjs engine.cpp -s "NO_EXIT_RUNTIME=1" -s "EXPORTED_RUNTIME_METHODS=['ccall']"
